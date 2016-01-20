80ab17094e43414cd5ed19f53db9f03697876ba8
Iliya Kuznetsov <iliya.kuznetsov@gmail.com>
2016-01-20 20:47:05 +0300

    v0.0.1
    v0.0.1

--- src/com/pinternals/ir/NotesDB.java
+++ src/com/pinternals/ir/NotesDB.java
@@ -17,8 +17,6 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
-import javafx.util.Pair;
-
 import org.apache.commons.lang3.StringEscapeUtils;
 
 import com.sap.JaxbNote;
@@ -72,23 +70,91 @@ enum ECat {
 	}
 }
 
-//class AppArea {
-//	String text, area, parent;
-//	static Map<String,Integer> qq = new HashMap<String,Integer>();
-//	static void put(String area, int db, int parent) {
-//		qq.put(area, db);
-//	}
-//	static int find(String area) {
-//		return qq.get(area);
-//	}
-//	AppArea getParent() {
-//		return null;
-//	}
-//}
+class Area {
+	protected static Map<String,Integer> areaToCode = new HashMap<String,Integer>();
+	protected static Map<String,String> areaToDescr = new HashMap<String,String>();
+	private static Set<String> zz2 = new HashSet<String>();
+	private static PreparedStatement aa02get, aa02put, aa02upd;
+	private static boolean aa02 = false;
+	static void init(NotesDB db) throws SQLException {//PreparedStatement aa02ge, PreparedStatement aa02pu, PreparedStatement aa02up) {
+		aa02get = db.sql("02appareaget");
+		aa02put = db.sqlUpd("02appareaput");
+		aa02upd = db.sqlUpd("02appareaupd");
+	}
+	static void reload() throws SQLException {
+		ResultSet rs = aa02get.executeQuery();
+		areaToCode.clear();
+		areaToDescr.clear();
+		zz2.clear();
+		while (rs.next()) {
+			areaToCode.put(rs.getString(1), rs.getInt(2));
+			areaToDescr.put(rs.getString(1), rs.getString(3));
+		}
+		rs.close();
+	}
+	static void handleSupport(String xa) throws SQLException {
+		if (!areaToCode.containsKey(xa) && !zz2.contains(xa)) {
+			aa02put.setString(1, xa);
+			aa02put.setString(2, null);	// descr is not here
+			aa02put.addBatch();
+			zz2.add(xa);
+		}
+	}
+	static void updateSupport() throws SQLException {
+		if (zz2.size()>0) {
+			int ia[] = aa02put.executeBatch();
+			assert zz2.size()==ia.length;
+			aa02put.getConnection().commit();
+			aa02put.clearBatch();
+			reload();
+		}
+		assert zz2.size()==0;
+	}
+	static Integer getCode(String a) {
+		return areaToCode.get(a);
+	}
+	static void handleLaunchpad(String xa, String descr) throws SQLException {
+		boolean b = areaToCode.containsKey(xa);
+		String sd = areaToDescr.get(xa); 
+		if (!b && !zz2.contains(xa)) {
+			aa02put.setString(1, xa);
+			aa02put.setString(2, descr);
+			aa02put.addBatch();
+			zz2.add(xa);
+		} else if (b && !descr.equals(sd)) {
+			aa02upd.setString(1,xa);
+			aa02upd.setString(2,descr);
+			aa02upd.addBatch();
+			areaToDescr.put(xa, descr);
+			aa02 = true;
+		}
+	}
+	static void updateLaunchpad() throws SQLException {
+		boolean x = aa02;
+		int ia[];
+		if (zz2.size()>0) {
+			ia = aa02put.executeBatch();
+			assert zz2.size()==ia.length;
+			x = true;
+		}
+		if (aa02) {
+			ia = aa02upd.executeBatch();
+			assert ia.length>0;
+			aa02 = false;
+		}
+		if (x) {
+			aa02put.getConnection().commit();
+			aa02put.clearBatch();
+			aa02upd.clearBatch();
+			reload();
+		}
+		assert zz2.size()==0 && !aa02;
+	}
+}
 
 public class NotesDB {
-//	private static Charset utf8 = Charset.forName("UTF-8");
-	Connection con = null;
+	private static final int SAP_KBA=1, SAP_UNKNOWNYET=0;
+	private Connection con = null;
 	private int eu;	// for asserts
 	private static ResourceBundle ddlrb = ResourceBundle.getBundle("com/pinternals/ir/ddl");
 	private static ResourceBundle sqlrb = ResourceBundle.getBundle("com/pinternals/ir/sql");
@@ -96,9 +162,11 @@ public class NotesDB {
 	private static String ddl(String code) {
 		return ddlrb.getString(code);
 	}
-
 	protected PreparedStatement sql(String code) throws SQLException {
-		return con.prepareStatement(sqlrb.getString(code));
+		return con.prepareStatement(sqlrb.getString(code), ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY, ResultSet.CLOSE_CURSORS_AT_COMMIT);
+	}
+	protected PreparedStatement sqlUpd(String code) throws SQLException {
+		return con.prepareStatement(sqlrb.getString(code), ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY, ResultSet.CLOSE_CURSORS_AT_COMMIT);
 	}
 
 	static void initDB(Path db) throws SQLException {
@@ -170,10 +238,10 @@ public class NotesDB {
 	private static final int bsmaxnotes = 3*1024*1024;
 	private BitSet bsnums = null, bs; // note numbers: 0 for absence, 1 for presence
 	Map<String,BitSet> bstitlang = null; //;
-	private PreparedStatement aa02get, aa02put, zzz2, zy2, zy3, fcupd, fcins;
-	Map<String,Integer> areaToCode = new HashMap<String,Integer>();
+	private PreparedStatement zzz2=null, zy2=null, zy3=null, fcupd=null, fcins=null;
 	Map<String,Integer> prio = new HashMap<String,Integer>();
 	Map<String,Integer> cat = new HashMap<String,Integer>();
+	Map<String,Integer> types = new HashMap<String,Integer>();
 	Set<String> zz2 = new HashSet<String>();
 	void walk22(Snotes sn, boolean isnew, String fname, String lastmod) throws SQLException, IOException {
 		if (sn==null) {
@@ -186,17 +254,18 @@ public class NotesDB {
 			bstitlang.put("E", new BitSet(bsmaxnotes));
 			bstitlang.put("D", new BitSet(bsmaxnotes));
 			bstitlang.put("J", new BitSet(bsmaxnotes));
-			aa02get = sql("02appareaget");
-			aa02put = sql("02appareaput");
+
+			Area.init(this);
+			Area.reload();
+
 			zzz2 = sql("zz2");
 			zy3 = sql("zy3");
 			zy2 = sql("zy2");
-			fcupd = sql("009fcupd");
-			fcins = sql("009fcins");
+			fcupd = sqlUpd("009fcupd");
+			fcins = sqlUpd("009fcins");
 		} 
 		pairToMap3(sql("01prioget2"), prio);
 		pairToMap3(sql("01catget2"), cat);
-		pairToMap3(aa02get, areaToCode);
 		ResultSet rs = sql("zzz").executeQuery();
 		while (rs.next()) bsnums.set(rs.getInt(1));
 		
@@ -206,22 +275,9 @@ public class NotesDB {
 			bs = bstitlang.get(lang);
 			while (rs.next()) bs.set(rs.getInt(1));
 		}
-		for (JaxbNote x: sn.getNS()) {
-			String xa = x.getA();
-			if (!areaToCode.containsKey(xa) && !zz2.contains(xa)) {
-				aa02put.setString(1, xa);
-				aa02put.addBatch();
-				zz2.add(xa);
-			}
-		}
-		if (zz2.size()>0) {
-			int ia[] = aa02put.executeBatch();
-			assert zz2.size()==ia.length;
-			con.commit();
-			aa02put.clearBatch();
-			pairToMap3(aa02get, areaToCode);
-			zz2.clear();
-		}
+		for (JaxbNote x: sn.getNS()) 
+			Area.handleSupport(x.getA());
+		Area.updateSupport();
 		for (JaxbNote x: sn.getNS()) {
 			String xa = x.getA(), xp = x.getP(), xc = x.getC(), xo = x.getO(), xl = x.getL(), xm = x.getM(), xcon = x.getContent();
 			int num = x.getN();
@@ -235,7 +291,7 @@ public class NotesDB {
 				xo = "Z" + num;
 			}
 			assert xo!=null;
-			Integer area = areaToCode.get(xa);
+			Integer area = Area.getCode(xa);
 			assert area!=null : num + "\tArea unknown: " + xa;
 			bs = bstitlang.get(xl);
 			if (!bsnums.get(num)) {
@@ -245,9 +301,9 @@ public class NotesDB {
 				zzz2.setInt(3, ca);
 				zzz2.setInt(4, pr);
 				if (xm!=null && xm.equals("*"))
-					zzz2.setInt(5, 2);
+					zzz2.setInt(5, SAP_KBA);
 				else if (xm==null)
-					zzz2.setInt(5, 1);
+					zzz2.setInt(5, SAP_UNKNOWNYET);
 				else
 					assert false : "Unknown mark: " + xm;
 				zzz2.setString(6, xo);
@@ -281,224 +337,6 @@ public class NotesDB {
 		con.commit();
 	} // void walk22(Iterator<Path> it);
 
-	List<Pair<String,String>> getAreas() throws SQLException {
-		List<Pair<String,String>> a = new ArrayList<Pair<String,String>>(25000);
-		ResultSet rs = sql("02apparea2").executeQuery();
-		while (rs.next()) { 
-			a.add(new Pair<String, String>(rs.getString(1),rs.getString(2)));
-		}
-		rs.close();
-		return a;
-	}
-	
-//	void walk(Iterator<Path> it, boolean force, Path d2) throws SQLException, IOException {
-//		PreparedStatement aa02get = sql("02appareaget");
-//		PreparedStatement aa02put = sql("02appareaput");
-//		Map<String,Integer> zz = new HashMap<String,Integer>();
-//		Map<String,Integer> prio = new HashMap<String,Integer>();
-//		pairToMap3(sql("01prioget2"), prio);
-//		Map<String,Integer> cat = new HashMap<String,Integer>();
-//		pairToMap3(sql("01catget2"), cat);
-//		Set<String> zz2 = new HashSet<String>();
-//		pairToMap3(aa02get, zz);
-//
-//		Set<Integer> notes = new HashSet<Integer>(2000000);
-//		ResultSet rs = sql("zzz").executeQuery();
-//		while (rs.next()) notes.add(rs.getInt(1));
-//		rs.close();
-//
-//		PreparedStatement zzz2 = sql("zz2");
-//		PreparedStatement zy3 = sql("zy3");
-//		PreparedStatement fcget = sql("009fcget");
-//		PreparedStatement fcupd = sql("009fcupd");
-//		PreparedStatement fcins = sql("009fcins");
-//		BufferedWriter pw = Files.newBufferedWriter(d2, csUtf8, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.APPEND); 
-//		while (it.hasNext()) {
-//			long lastmodDb = 0;
-//			Path p = it.next();
-//			long lastmodFile = Files.getLastModifiedTime(p).toMillis();
-//
-//			String pn = p.getFileName().toString();
-//			fcget.setString(1, pn);
-//			rs = fcget.executeQuery();
-//			while (rs.next()) lastmodDb = rs.getLong(1); 
-//
-//			if (!force && lastmodFile == lastmodDb) continue;
-//			System.out.println(pn + " at db=" + lastmodDb + ", at fs=" + lastmodFile);
-//			JaxbSnotes sn = JAXB.unmarshal(p.toFile(), JaxbSnotes.class);
-//			assert sn!=null && sn.n!=null && sn.n.size()>0;
-//
-//			String lang = sn.n.get(0).l;
-//			PreparedStatement zy2 = sql("zy2");
-//			Set<Integer> nol = new HashSet<Integer>(2000000);
-//
-//			zy2.setString(1, lang);
-//			rs = zy2.executeQuery();
-//			while (rs.next()) nol.add(rs.getInt(1));
-//			rs.close();
-//
-//			for (JaxbNote x: sn.n) {
-//				if (!zz.containsKey(x.a) && !zz2.contains(x.a)) {
-//					aa02put.setString(1, x.a);
-//					aa02put.addBatch();
-//					zz2.add(x.a);
-//				}
-//			}
-//			if (zz2.size()>0) {
-//				int ia[] = aa02put.executeBatch();
-//				assert zz2.size()==ia.length;
-//				con.commit();
-//				aa02put.clearBatch();
-//				pairToMap3(aa02get, zz);
-//				zz2.clear();
-//			}
-//			boolean itz = false, it3 = false;
-//			for (JaxbNote x: sn.n) {
-//				Integer pr = prio.get(x.p);
-//				Integer ca = cat.get(x.c);
-//				if (pr==null) System.err.println(x.n + "\tPriority unknown: " + x.p);
-//				if (ca==null) System.err.println(x.n + "\tCategory unknown: " + x.c);
-//				assert (pr!=null && ca!=null);
-//				if (x.o==null) {
-//					x.o = "Z" + x.n;
-//				}
-//				assert x.o!=null;
-//				Integer area = zz.get(x.a);
-//				assert area!=null : x.n + "\tArea unknown: " + x.a;
-//				String s = x.n + "\t" + x.a + "\t" + x.d + "\t" + x.l + "\n";
-//				if (!notes.contains(x.n)) {
-//					zzz2.setInt(1, x.n);
-//					zzz2.setInt(2, area);
-//					zzz2.setInt(3, ca);
-//					zzz2.setInt(4, pr);
-//					if (x.m!=null && x.m.equals("*"))
-//						zzz2.setInt(5, 2);
-//					else if (x.m==null)
-//						zzz2.setInt(5, 1);
-//					else
-//						assert false : "Unknown mark: " + x.m;
-//					if (x.o!=null)
-//						zzz2.setString(6, x.o);
-//					else
-//						zzz2.setNull(6, Types.VARCHAR);
-//					zzz2.addBatch();
-//					notes.add(x.n);
-//					itz = true;
-//					zy3.setInt(1, x.n);
-//					zy3.setString(2, x.l);
-//					zy3.setString(3, x.content);
-//					zy3.addBatch();
-//					nol.add(x.n);
-//					it3 = true;
-//					pw.write(s);
-//				} else if (!nol.contains(x.n)) {
-//					zy3.setInt(1, x.n);
-//					zy3.setString(2, x.l);
-//					zy3.setString(3, x.content);
-//					zy3.addBatch();
-//					it3 = true;
-//					nol.add(x.n);
-//					pw.write(s);
-//				} 
-//			} // for (JaxbNote x: sn.n) 
-//			if (itz) {
-//				zzz2.executeBatch();
-//				zzz2.clearBatch();
-//			}
-//			if (it3) {
-//				zy3.executeBatch();
-//				zy3.clearBatch();
-//			}
-//			con.commit();
-//			if (lastmodDb==0) {
-//				fcins.setString(1, pn);
-//				fcins.setLong(2, lastmodFile);
-//				fcins.executeUpdate();
-//			} else {
-//				fcupd.setString(1, pn);
-//				fcupd.setLong(2, lastmodFile);
-//				fcupd.executeUpdate();
-//			}
-//			con.commit();
-//			pw.flush();
-//		}
-//		pw.close();
-//	} // void walk(Iterator<Path> it);
-
-//	void skywalk(String area, PrintWriter pw) throws SQLException {
-//		PreparedStatement ps = sql("sw01");
-//		ps.setString(1, area);
-//		ResultSet rs = ps.executeQuery();
-//		while (rs.next()) {
-//			String x = rs.getString(2);
-//			pw.println(String.format("%07d\t%s\t%s", rs.getInt(1), x!=null?x:"", rs.getString(3)));
-//		}
-//		pw.flush();
-//		rs.close();
-//	} // void skywalk() throws SQLException {
-	
-//	void skywalk2(String area, PrintWriter pw) throws SQLException {
-//		PreparedStatement ps = sql("sw02");
-//		ps.setString(1, area);
-//		ResultSet rs = ps.executeQuery();
-//		while (rs.next()) {
-//			String x = rs.getString(2);
-//			pw.println(String.format("%07d\t%s\t%s", rs.getInt(1), x!=null?x:"", rs.getString(3)));
-//		}
-//		pw.flush();
-//		rs.close();
-//	} // void skywalk2() throws SQLException {
-
-//	List<Integer> getUnknownObjid(String o, int top) throws SQLException {
-//		List<Integer> a = new ArrayList<Integer>(100);
-//		PreparedStatement ps = sql("sw03");
-//		ps.setString(1, o);
-////		ps.setInt(2,  top);
-//		ResultSet rs = ps.executeQuery();
-//		while (rs.next()) a.add(rs.getInt(1));
-//		rs.close();
-//		ps.close();
-//		return a;
-//	}
-
-//	int updateObjid(List<Integer> num, List<String> obj) throws SQLException {
-//		int x = num.size(), i = 0, p=999, q = 0;
-//		assert obj.size()==x;
-//		PreparedStatement ps = sql("sw04");
-//		for (String o: obj) {
-//			int j = num.get(i++);
-//			if (o!=null) {
-//				ps.setInt(1, j);
-//				ps.setString(2, o);
-//				ps.addBatch();
-//				q++;
-//			}
-//			if (p--==0) {
-//				ps.executeBatch();
-//				con.commit();
-//				ps.clearBatch();
-//				p=999;
-//			}
-//		}
-//		ps.executeBatch();
-//		con.commit();
-//		ps.close();
-//		return q;
-//	}
-	
-//	void report(String b) throws SQLException {
-//		ResultSet rs = sql(b).executeQuery();
-//		while (rs.next()) {
-//			String a = rs.getString(1);
-//			String q = rs.getString(2);
-//			int p = rs.getInt(3);
-////			int r = rs.getInt(4);
-////			int s = rs.getInt(5);
-//			System.out.println(a + "\t" + q + "\t" + p);
-//		}
-//		rs.close();
-//	}
-	
 	List<AZ> getZ3(String area) throws SQLException, ParseException {
 		List<AZ> az = new ArrayList<AZ>(1000);
 		PreparedStatement ps = sql("w07");
@@ -533,7 +371,6 @@ public class NotesDB {
 				p.setComponentText(rs.getString(10));
 				p.setLanguage(rs.getString(11));
 				az.foundA(rs.getString(1), p);
-//				az.foundA(rs.getString(1), rs.getString(2));
 			}
 		}
 	}
@@ -558,39 +395,154 @@ public class NotesDB {
 		assert eu == 1;
 		con.commit();
 	}
-//	private PreparedStatement zy5 = null;
-//	void setMissedTitle(int num, String lang) throws SQLException {
-//		assert num > 0 && lang!=null;
-//		assert "DEJ".contains(lang);
-//		if (zy5==null) zy5 = sql("zy5");
-//		zy5.setInt(1, num);
-//		zy5.setString(2, lang);
-//		zy5.executeUpdate();
-//		con.commit();
-//	}
+
+	void merge(NotesDB dba) throws SQLException {
+		// dba == area db
+		assert dba!=null && dba.con!=null && !dba.con.isClosed();
+		PreparedStatement px = dba.con.prepareStatement("select NotesNumber,Version,NotesKey,"
+				+ "Title,Type,Priority,Category,ReleasedOn,ComponentKey,ComponentText,Language,askdate from a01");
+		PreparedStatement mr01 = sql("mr01"), mr02 = sqlUpd("mr02"), zzz2 = sqlUpd("zz2"), zy3 = sqlUpd("zy3");
+		PreparedStatement mr05 = sqlUpd("mr05"), mr08 = sql("mr08"), mr09 = sqlUpd("mr09");
+		ResultSet rx = px.executeQuery(), rs;
+		pairToMap3(sql("01prioget2"), prio);
+		pairToMap3(sql("01catget2"), cat);
+		pairToMap3(sql("01types"), types);
+		Area.init(this);
+		Area.reload();
+		
+		// 1st -- areas handling and preparation   TODO rewrite to distinct
+		while (rx.next()) 
+			Area.handleLaunchpad(rx.getString(9), rx.getString(10));
+		Area.updateLaunchpad();
+		rx = px.executeQuery();
+
+		bsnums = new BitSet(bsmaxnotes);
+		bstitlang = new HashMap<String,BitSet>();
+		bstitlang.put("E", new BitSet(bsmaxnotes));
+		bstitlang.put("D", new BitSet(bsmaxnotes));
+		bstitlang.put("J", new BitSet(bsmaxnotes));
+		zy2 = sql("zy2");
+		for (String lang: bstitlang.keySet()) {
+			zy2.setString(1, lang);
+			rs = zy2.executeQuery();
+			bs = bstitlang.get(lang);
+			while (rs.next()) bs.set(rs.getInt(1));
+		}
+
+		// 2nd -- notes handling
+		while (rx.next()) {
+			String ad, nk, title, type, pris, cas, relon, lang; 
+			int num = rx.getInt(1), ver = rx.getInt(2), ac = Area.getCode(rx.getString(9)).intValue();
+			nk = rx.getString(3);
+			title = rx.getString(4);
+			type = rx.getString(5);
+			pris = rx.getString(6);
+			cas = rx.getString(7);
+			relon = rx.getString(8);
+			lang = rx.getString(11);
+			ad = rx.getString(12);
+			
+			mr01.setInt(1, num);
+			rs = mr01.executeQuery();
+			while (rs.next()) {
+				bsnums.set(num);
+				String objid = rs.getString(1);
+				int ar = rs.getInt(2), ca = rs.getInt(3), pr = rs.getInt(4), m = rs.getInt(5);
+				boolean b = ac!=ar;
+				b = b || cat.get(cas)!=ca;
+				b = b || prio.get(pris)!=pr;
+				b = b || types.get(type)!=m;
+				b = b || !objid.equals(nk);
+				if (b) {
+					mr02.setInt(1, num);
+					mr02.setString(2, nk);
+					mr02.setInt(3, ac);
+					mr02.setInt(4, cat.get(cas));
+					mr02.setInt(5, prio.get(pris));
+					mr02.setInt(6, types.get(type));
+					mr02.addBatch();
+				}
+			}
+			rs.close();
+			if (!bsnums.get(num)) {
+				zzz2.setInt(1, num);
+				zzz2.setInt(2, ac);
+				zzz2.setInt(3, cat.get(cas));
+				zzz2.setInt(4, prio.get(pris));
+				zzz2.setInt(5, types.get(type));
+				zzz2.setString(6, nk);
+				zzz2.addBatch();
+				bsnums.set(num);
+			}
+			if (!bstitlang.get(lang).get(num)) {
+				zy3.setInt(1, num);
+				zy3.setString(2, lang);
+				zy3.setString(3, title);
+				zy3.addBatch();
+				bstitlang.get(lang).set(num);
+				mr05.setInt(1,num);
+				mr05.setString(2, lang);
+				mr05.setInt(3, ver);
+				mr05.setString(4, relon);
+				mr05.setString(5, ad);
+				mr05.addBatch();
+			} else {
+				mr08.setInt(1, num);
+				mr08.setString(2, lang);
+				rs = mr08.executeQuery();
+				boolean b = false;
+				while (rs.next()) {
+					int ver2 = rs.getInt(1);
+					String relon2 = rs.getString(2), ad2 = rs.getString(3);
+					if (ver==ver2) {
+						if (!ad2.equals(ad) || relon2.equals(relon)) {
+							mr09.setInt(1, num);
+							mr09.setString(2, lang);
+							mr09.setInt(3, ver);
+							mr09.setString(4, relon);
+							mr09.setString(5, ad);
+							mr09.addBatch();
+						}
+						b = true;
+					}
+				}
+				rs.close();
+				if (!b) { 
+					mr05.setInt(1,num);
+					mr05.setString(2, lang);
+					mr05.setInt(3, ver);
+					mr05.setString(4, relon);
+					mr05.setString(5, ad);
+					mr05.addBatch();
+				}				
+			}
+		}
+		px.close();
+		int ia[] = zzz2.executeBatch();
+		ia = mr02.executeBatch();
+		ia = zy3.executeBatch();
+		ia = mr05.executeBatch();
+		ia = mr09.executeBatch();
+		con.commit();
+		System.out.println(ia);
+	}
 	
-//	private PreparedStatement zy6 = null, v02=null;
-//	void updateVer(AZ az) throws SQLException {
-//		assert !az.badLang;
-//		assert az.objid!=null;
-//		if (zy6==null) zy6 = sql("zy6");
-//		// update objid
-//		if (!az.objid.equals(az.objid2)) {
-//			zy6.setInt(1, az.num);
-//			zy6.setString(2, az.objid2);
-//			eu = zy6.executeUpdate();
-//			assert eu==1;
-//			az.objid2 = az.objid;
-//			con.commit();
-//		}
-//		if (v02==null) v02 = sql("v02");
-//		v02.clearParameters();
-//		v02.setInt(1, az.num);
-//		v02.setString(2, az.lang);
-//		v02.setInt(3, Integer.valueOf(az.newVers));
-//		v02.setString(4, az.newReleasedOn);
-//		eu = v02.executeUpdate();
-//		assert eu==1;
-//		con.commit();
-//	}
+	List<Integer> arrrrrrrrrr(List<String> as, String pat) throws SQLException {
+		List<Integer> ar = new ArrayList<Integer>(10000);
+		PreparedStatement ps = sql("03areaunk");
+		ps.setString(1, pat);
+		ResultSet rs = ps.executeQuery();
+		String p = null;
+		while (as.size()>0 && rs.next()) {
+			int num = rs.getInt(1);
+			String t = rs.getString(2);
+			if (p!=t && as.contains(t)) {
+				System.out.println(num + "\t" + t);
+				ar.add(num);
+				as.remove(t);
+				p = t;
+			}
+		}
+		return ar;
+	}
 }
