ec1949110b094d8f7ae1b0851adc8e389c4e07d5
Iliya Kuznetsov <iliya.kuznetsov@gmail.com>
2016-01-21 20:59:30 +0300

    v0.0.2
    v0.0.2 -- more elements added

--- src/xsd.bat
+++ src/xsd.bat
@@ -1,4 +1,5 @@
 @echo off
-"c:\Program Files\Java\jdk1.8.0_66\bin\xjc" -mark-generated -extension -p com.sap xsd
-rem "c:\Program Files\Java\jdk1.8.0_66\bin\xjc" -p com.sap xsd
-
+"c:\Program Files\Java\jdk1.8.0_66\bin\xjc" -mark-generated -extension -p com.sap xsd 
+"c:\Program Files\Java\jdk1.8.0_66\bin\xjc" -mark-generated -extension -p com.sap1 xsd_abap1 
+"c:\Program Files\Java\jdk1.8.0_66\bin\xjc" -mark-generated -extension -p com.sap2 xsd_abap2
+"c:\Program Files\Java\jdk1.8.0_66\bin\xjc" -mark-generated -extension -p com.sap3 xsd_abap3
